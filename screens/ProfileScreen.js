2bf261317055ce8d55e380725784af8b4f07192c
Adeline Liou <adeline@usc.edu>
2022-11-12 16:14:31 -0800

    Updated Profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,6 +3,8 @@ import React, {useState} from 'react';
 import { Image, View,StyleSheet, Text, Dimensions } from 'react-native';
 import Slider from '@react-native-community/slider'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import { disableExpoCliLogging } from 'expo/build/logs/Logs';
+import warningIcon from '../assets/warningicon.svg';
 
 function ProfileScreen(props) {
 
@@ -12,18 +14,28 @@ function ProfileScreen(props) {
     return (
         <NativeBaseProvider>
           <View style = {styles.container}>
-              <Text style = {styles.title}>Adjust Tommy's Credit Limit</Text>
-              <Text style={{fontSize:15, fontWeight:"bold", paddingTop:10}}>${Math.floor(range*300)}</Text>
-              <Slider
-                style={{width:300,height:50, color:"#C2D76E"}}
-                onValueChange = {(value)=>setRange(value)}
-                minimumValue = {0}
-                maximumValue = {1}
-                thumbTintColor = 'white'
-                maximumTrackTintColor='#A0D99530'
-                minimumTrackTintColor='#A0D995'
-                outli
-              />
+            <Text style = {styles.title}>Profile</Text>
+            <Text style = {styles.overview_text}>Current Credit Score</Text>
+            <View style={styles.circle}> 
+                <Text style={styles.score}>714</Text>
+            </View>
+            <Text style = {styles.overview_text}>Adjust Tommy's Credit Limit</Text>
+            
+            <View style={styles.square}> 
+                <Text style={styles.dollar_text}>${Math.floor(range*300)}</Text>
+            </View>
+            <Slider
+            style={styles.slider_style}
+            onValueChange = {(value)=>setRange(value)}
+            minimumValue = {0}
+            maximumValue = {1}
+            thumbTintColor = 'white'
+            maximumTrackTintColor='#A0D99530'
+            minimumTrackTintColor='#A0D995'
+            />
+            {/* <Text style = {styles.overview_text}>Recent Notifications</Text> */}
+            <Example/>
+            
           </View>
             
         </NativeBaseProvider>
@@ -32,35 +44,161 @@ function ProfileScreen(props) {
     );
 }
 
+const Example = () => {
+    const data = [{
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      fullName: "Review Action",
+      timeStamp: "12:47 PM",
+      recentText: "Child in Yellow Zone for food!",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }, {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      fullName: "Review Action",
+      timeStamp: "11:11 PM",
+      recentText: "Child in Red Zone for clothing!",
+      avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
+    }, {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      fullName: "Review Action",
+      timeStamp: "6:22 PM",
+      recentText: "Child in Yellow Zone for food!",
+      avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
+    }, {
+      id: "68694a0f-3da1-431f-bd56-142371e29d72",
+      fullName: "Review Action",
+      timeStamp: "8:56 PM",
+      recentText: "Transaction above limit attempted!",
+      avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+    }, {
+        id: "68694a0f-3da1-431f-bd56-142371e29d72",
+        fullName: "Review Action",
+        timeStamp: "8:56 PM",
+        recentText: "Transaction above limit attempted!",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+      }, {
+        id: "68694a0f-3da1-431f-bd56-142371e29d72",
+        fullName: "Review Action",
+        timeStamp: "8:56 PM",
+        recentText: "Transaction above limit attempted!",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+      }, {
+        id: "68694a0f-3da1-431f-bd56-142371e29d72",
+        fullName: "Review Action",
+        timeStamp: "8:56 PM",
+        recentText: "Transaction above limit attempted!",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+      }, {
+        id: "68694a0f-3da1-431f-bd56-142371e29d72",
+        fullName: "Review Action",
+        timeStamp: "8:56 PM",
+        recentText: "Transaction above limit attempted!",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+      }, 
+    ];
+    return(
+        <Box height ={200}>
+        <Text style= {styles.box_textTitle}>Recent Notifications</Text>
+        <FlatList data={data} renderItem={({item
+      }) => <Box borderWidth="0" mbackgroundColor="muted.200" _dark={{
+        borderColor: "muted.300"
+      }} borderColor="muted.300" borderRadius= "10" pl={["1", "1"]} pr={["1", "1"]} py="1">
+              <HStack space={[3, 3]} justifyContent="space-between">
+                <Avatar size="30px" source={{
+            uri: item.avatarUrl
+          }} />
+                <VStack>
+                  <Text _dark={{
+              color: "muted.300"
+            }} color="muted.300" bold>
+                    {item.fullName}
+                  </Text>
+                  <Text color="coolGray.600" _dark={{
+              color: "warmGray.200"
+            }}>
+                    {item.recentText}
+                  </Text>
+                </VStack>
+                <Spacer />
+                <Text fontSize="xs" _dark={{
+            color: "warmGray.50"
+          }} color="coolGray.800" alignSelf="flex-start">
+                  {item.timeStamp}
+                </Text>
+              </HStack>
+            </Box>} keyExtractor={item => item.id} />
+      </Box>);
+    
+  };
+
 const styles = StyleSheet.create({
     title: {
-        fontSize: 20,
-        color: 'grey',
-        alignItems: 'left'
-
-    },
-    card_image:{
-        width: '100%',
-        height: '35%',
-        // backgroundColor: "green",
-        top: 20,
-        bottom: 20
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'black',
+        left: 20,
+        alignSelf: 'left',
+        paddingBottom: 5
     },
     container: {
-        alignItems: 'center',
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        alignItems:'center'
     },
-    card_text: {
-        left: '-40%',
-        top: 5,
-        color:"grey"
+    circle:{
+        width: 150,
+        height: 150,
+        paddingTop:20,
+        paddingBottom:30,
+        borderRadius: 150 / 2,
+        backgroundColor: '#A0D995',
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    square:{
+        width: 50,
+        height: 30,
+        borderRadius: 20 / 8,
+        backgroundColor: '#A0D995',
+        justifyContent: 'center'
     },
     overview_text: {
-        left: '-36%',
-        top: 20,
-        color:"grey"
+        paddingTop: 10,
+        paddingBottom:10,
+        color:"grey",
+        alignItems:'left',
+        paddingLeft: 20,
+        alignSelf: 'left',
     },
+    box_textTitle: {
+        paddingTop: 10,
+        paddingBottom:10,
+        color:"grey",
+        alignItems:'left',
+        alignSelf: 'left',
+    },
+    dollar_text:{
+        fontSize:15,
+        color:'white',
+        textAlign:'center'
+    },
+    slider_style:{
+        width:300,
+        height:50, 
+        color:"#C2D76E",
+        paddingTop: 10
+    },
+    score:{
+        fontSize:35,
+        color:'white',
+        textAlign: 'center',
+        alignItems:'center'
+    }
  
 })
 
